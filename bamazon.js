ff450216a10c3eec54039528597390890e9f2745
Marie Nabors <marienabors@thefirstacademy.org>
2018-02-15 18:47:06 -0500

    indentation

--- bamazon.js
+++ bamazon.js
@@ -66,34 +66,35 @@ var askShopper = function(res){
 //for loop checks to see if the user's response matches any of the choices 
 //in the item_name array
 					for(var i=0; i<res.length;i++){
-						if(res[i].id==answer.choice){
-							correct=true;
-							var selection=answer.choice;
-							var id=i+1;
+					if(res[i].id==answer.choice){
+					correct=true;
+					var selection=answer.choice;
+					var id=i+1;
 			}
 
 		}
-		//query database for stock inventory of chosen item to buy
-		var query = "SELECT * FROM products";
+
+//query database for stock inventory of chosen item to buy
+var query = "SELECT * FROM products";
      connection.query(query,function(err, res) {
         for (var i = 0; i < res.length; i++) {
-          //console.log(res[i]);
-          //check if quantity desired is less than available stock inventory for that item
-          if (answer.quantity<res[answer.choice-1].stock_inventory){
-          	//subtract quantity desired from inventory for that item
+//console.log(res[i]);
+//check if quantity desired is less than available stock inventory for that item
+         if (answer.quantity<res[answer.choice-1].stock_inventory){
+//subtract quantity desired from inventory for that item
           	var newStockInventory = res[answer.choice-1].stock_inventory-answer.quantity;
-          	//console.log("******"+newStockInventory);
-          }
-          var update = connection.query("UPDATE products SET stock_inventory = '" + (res[id].stock_inventory - answer.quantity)
-      //var update = connection.query("UPDATE products SET stock_inventory = " (res[id].stock_inventory-answer.quantity)
-      + "'", function(err, res2){
-      	console.log("Enjoy your fabulous purchase!")
-      	//displayTable();
-      })
-      	//+ newStockInventory + " WHERE id = " + answer.choice
+//console.log("******"+newStockInventory);
+          	}
+          	var update = connection.query("UPDATE products SET stock_inventory = '" + (res[id].stock_inventory - answer.quantity)
+//var update = connection.query("UPDATE products SET stock_inventory = " (res[id].stock_inventory-answer.quantity)
+      		+ "'", function(err, res2){
+      		console.log("Enjoy your fabulous purchase!")
+//displayTable();
+     		 })
+ //+ newStockInventory + " WHERE id = " + answer.choice
 			connection.query(query,function(err, res) {
 			//	console.log(res);
-})
+			})
       };
 	})
  })
