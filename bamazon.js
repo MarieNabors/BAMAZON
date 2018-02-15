f856c821dca21383a5b23642bf71d1a0972af07f
Marie Nabors <marienabors@thefirstacademy.org>
2018-02-14 21:50:18 -0500

    added functions

--- bamazon.js
+++ bamazon.js
@@ -7,18 +7,20 @@ var inquirer = require("inquirer");
 //connect to mySQLworkbench database, "Bamazon"
 
 var connection = mySQL.createConnection({
-	host: "localhost",
-	port: 3306,
-	user:"root",
-	password:"root",
-	database: "Bamazon"
-})
+			host: "localhost",
+			port: 3306,
+			user:"root",
+			password:"root",
+			database: "Bamazon"
+		})
 
 connection.connect(function(err){
 	if (err) throw err;
 	console.log("You successfully connected to your mySQL database!");
 	displayTable();
 })
+//print table to terminal
+//create a function to display the table to the user/shopper
 var displayTable = function(){
 	connection.query("SELECT * FROM products", function(err, res){
 		for(var i=0; i<res.length; i++){
@@ -29,25 +31,60 @@ var displayTable = function(){
 		askShopper(res);
 	})
 }
-
-var askShopper = function(res){
-	inquirer.prompt([{
-		type:"input",
-		name:"choice",
-		message:"Would you like to buy an item?"
-	}]).then(function(answer){
-		var correct = false;
-		for(var i=0; i<res.length;i++){
-			if(res[i].item_name==answer.choice){
-				correct=true;
-				var selection=answer.choice;
-				var id=i;
-			}
-		}
-	}
 //ask user if they want to purchase an item
 //give user the option to purchase an item
-//ask user how many of their option
+//ask user how many they want to buy
+
+
+var askShopper = function(res){
+				inquirer.prompt([{
+					type:"input",
+					name:"choice",
+					message:"What would you like to buy? Please refer to item number."
+				}, 
+				{	
+					type:"input",
+					name:"quantity",
+					message:"How many would you like?"
+				}])
+				.then(function(answer){
+					var correct = false;
+//^^^^creates a prompt function to ask the user what they want to buy
+
+//for loop checks to see if the user's response matches any of the choices 
+//in the item_name array
+					for(var i=0; i<res.length;i++){
+						if(res[i].id==answer.choice){
+							correct=true;
+							var selection=answer.choice;
+							var id=i+1;
+			}
+
+		}
+		//query database for stock inventory of chosen item to buy
+		var query = "SELECT * FROM products";
+     connection.query(query,function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          //console.log(res[i]);
+          //check if quantity desired is less than available stock inventory for that item
+          if (answer.quantity<res[answer.choice-1].stock_inventory){
+          	//subtract quantity desired from inventory for that item
+          	var newStockInventory = res[answer.choice-1].stock_inventory-answer.quantity;
+          	console.log("******"+newStockInventory);
+          }
+      var update = "UPDATE products SET stock_inventory =" + newStockInventory + " WHERE id = " + answer.choice
+			connection.query(query,function(err, res) {
+			//	console.log(res);
+})
+      };
+	})
+ })
+}
+
+//ask user how many they want to buy
+// check inventory for sufficient quantity
+//either respond "insufficient quantity" or fulfill the order and update inventory 
+
 //instantiating table, copy/pasted from npmjs
 // var table = new Table({
 //     head: ['TH 1 label', 'TH 2 label']
@@ -62,6 +99,6 @@ var askShopper = function(res){
  
 // console.log(table.toString());
 
-//create function to print table to terminal
+
 
 
